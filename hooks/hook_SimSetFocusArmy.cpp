c9e978dc1701e1d32df22333e4de58bb85fc9e53
KionX <5648030+KionX@users.noreply.github.com>
2019-11-29 08:45:51 +0500

    Add files via upload

---
+++ hooks/hook_SimSetFocusArmy.cpp
@@ -0,0 +1,16 @@
+//HOOK SetCommandSource ROffset = 0x00308A9A
+#include <stdlib.h>
+#include "../preprocessor/macro.h"
+
+__asm__
+(
+    ".equ reg_struct_address,"#SSFARegDesc" \n"
+);
+
+__asm__ volatile
+(   //00708A9A
+    "MOV EAX,reg_struct_address \n"
+    ".align 128, 0x0 \n"
+);
+
+
