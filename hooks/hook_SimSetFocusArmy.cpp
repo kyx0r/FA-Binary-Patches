41fd20f466866549cf8b32b81e399dfa1fb5e2c0
KionX <5648030+KionX@users.noreply.github.com>
2019-11-29 13:26:30 +0500

    Update hook_SimSetFocusArmy.cpp

--- hooks/hook_SimSetFocusArmy.cpp
+++ hooks/hook_SimSetFocusArmy.cpp
@@ -1,16 +1,18 @@
 //HOOK SetCommandSource ROffset = 0x00308A9A
 #include "../preprocessor/define.h"
 #include "../preprocessor/macro.h"
+#include "../sections/include/moho.h"
 
-__asm__
-(
-    ".equ reg_struct_address,"QU(SSFARegDesc)" \n"
-);
-
+luaFuncDescReg SSFARegDesc = {0x00E45E90,          // Std register func
+                              0x00E43408,          // "SetFocusArmy"
+                              0x00E00D90,          // "<global>"
+                              0x00E451FC,          // "SetFocusArmy(armyIndex or -1)"
+                              0x010B8AE8,          // Prev reg desc: ArmyGetHandicap
+                              QU(SimSetFocusArmy), // Func ptr
+                              0x00000000};         // C++ class vtable ptr
+                              
 __asm__ volatile
 (   //00708A9A
-    "MOV EAX,reg_struct_address \n"
+    "MOV EAX, SSFARegDesc \n"
     ".align 128, 0x0 \n"
 );
-
-
