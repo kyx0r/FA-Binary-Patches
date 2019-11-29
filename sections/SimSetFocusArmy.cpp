584e4001903291d939b2ef34b4bd1199b9f2c8c2
KionX <5648030+KionX@users.noreply.github.com>
2019-11-29 10:50:19 +0500

    Update SimSetFocusArmy.cpp

--- sections/SimSetFocusArmy.cpp
+++ sections/SimSetFocusArmy.cpp
@@ -5,13 +5,13 @@ void SimSetFocusArmy()
 {
 	__asm__
     (
-        "PUSH 1 \n"
+        "PUSH 0x1 \n"
         "PUSH ESI \n"
-        "CALL 0128B20C \n"     // lua_ToInt
-        "ADD ESP,8 \n"
-        "MOV ECX,[10C4F50] \n" // g_STIDriver
-        "MOV [ECX+0B0],EAX \n" // simFocusArmyIndex
-        "MOV EAX,1 \n"
+        "CALL 0x0128B20C \n"     // lua_ToInt
+        "ADD ESP,0x8 \n"
+        "MOV ECX,[0x10C4F50] \n" // g_STIDriver
+        "MOV [ECX+0x0B0],EAX \n" // simFocusArmyIndex
+        "MOV EAX,0x1 \n"
         "RET \n"
     );
 }
