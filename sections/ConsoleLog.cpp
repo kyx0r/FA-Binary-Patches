a81fb198573acfdc96bcc2c2b1efe4ff80ba688a
KionX <5648030+KionX@users.noreply.github.com>
2020-01-04 16:23:53 +0500

    Add files via upload

---
+++ sections/ConsoleLog.cpp
@@ -0,0 +1,11 @@
+void ConsoleLog()
+{
+    __asm__
+    (
+        "MOV DWORD PTR SS:[EBP-4],2 \n"
+        "PUSH [ESP+0xF4] \n"
+        "CALL 0x00937CB0 \n"
+        "ADD ESP,4 \n"
+        "JMP 0x0041CE11 \n"
+    );
+}
